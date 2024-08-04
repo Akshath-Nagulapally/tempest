de17db4f892775214ec2c47f55a1dcdb32604403
Akshath-Nagulapally <akshathnag04@gmail.com>
2024-08-04 12:32:31 +0530

    removed the weird section border glitch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,12 +26,12 @@ export default async function Index() {
   return (
 
     <div className="flex w-full flex-1 flex-col items-center gap-20">
-      <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
+      {/* <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10"> */}
         {/* <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm"> */}
         <div> 
           {isSupabaseConnected && <AuthButton />}
         </div>
-      </nav>
+      {/* </nav> */}
     <div>
         <Landing />
          <main className="flex flex-1 flex-col gap-6">
